e580846a2dc77bcc4a47c516fe49e896851e83cf
subinjeong123 <55428469+subinjeong123@users.noreply.github.com>
2019-11-21 20:38:54 +0900

    3-mix and put card

--- blackjack.c
+++ blackjack.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <time.h>
+#include <stdbool.h> 
 
 #define N_CARDSET			1
 #define N_CARD				52
@@ -10,10 +11,9 @@
 
 
 //card tray object
-int CardTray[N_CARDSET*N_CARD] = { 1,2,3,4,5,6,7,8,9,10,10,10,10,
-    1,2,3,4,5,6,7,8,9,10,10,10,10,
-    1,2,3,4,5,6,7,8,9,10,10,10,10,
-    1,2,3,4,5,6,7,8,9,10,10,10,10 };           //Leaving the role of the getCardnum function out to see if the program starts well. We're going to put it inside when it goes any further.
+int CardTray[N_CARDSET*N_CARD];    //Leaving the role of the getCardnum function out to see if the program starts well. We're going to put it inside when it goes any further.
+int swit[52];   
+int cardresult;    
 int Ace = 0;
 
 
@@ -148,6 +148,39 @@ void printCard(int cardnum)
 }
 
 
+//card array controllers -------------------------------
+
+//mix the card sets and put in the array
+void mixCardTray(int CardTray[N_CARDSET*N_CARD])
+{
+	int w = 0,r = 0;
+	int i = 0;
+	for(i = 0; i < 52; i++) 
+	{
+		swit[i] = false;
+	}
+
+	srand(time(NULL));
+	while(w < 52) 
+	{
+		r = rand() % 52;
+		if(swit[r] == false) 
+		{
+			swit[r] = true;
+			CardTray[w] = r + 1;
+			w++;
+		}
+	}
+}
+//get one card from the tray
+int pullCard(int cardresult)
+{
+	int i;
+	cardresult = swit[i];
+	i++;
+	return cardresult;
+}
+
 //playing game functions -----------------------------
 
 //player settiing
@@ -171,6 +204,7 @@ int main(void)
 	printCard(cardnum);
 	for (cardnum=0;cardnum<N_CARD;cardnum++)
     getCardNum(cardnum);
+    mixCardTray(CardTray);
 	return 0;
 }
 
